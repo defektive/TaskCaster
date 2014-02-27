e9926ec206f0ac5e21b2ee2d7fb729152e3fb551
Matt Zabriskie <mzabriskie@gmail.com>
2014-02-27 10:44:57 -0700

    Stubbing in apiService

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 (function () {
-	var API_CONTEXT = 'https://hub.attask.com/attask/api-internal/';
+	var API_CONTEXT = 'https://hub.attask.com/attask/api-internal';
 
 	angular.module('TaskCaster', ['ngRoute'])
 		.config(['$locationProvider', function ($locationProvider) {
@@ -10,10 +10,19 @@
 				.when('/', {templateUrl: 'src/partials/home.html', controller: 'HomeCtrl'})
 				.otherwise({templateUrl: 'src/partials/404.html'});
 		}])
-		.controller('HomeCtrl', function ($scope, $http) {
-			$http.jsonp(API_CONTEXT + 'OPTASK/search?listOptions={reportID:%20%22530e6a6d000d3c71b57dba2baf4ee95e%22}&filters={%22team:name%22:%22ISIS%22}&jsonp=JSON_CALLBACK')
+		.controller('HomeCtrl', function ($scope, apiService) {
+			apiService.search('OPTASK', 'listOptions={reportID:%20%22530e6a6d000d3c71b57dba2baf4ee95e%22}&filters={%22team:name%22:%22ISIS%22}')
 				.success(function (result) {
 					$scope.data = result.data;
 				});
+		})
+		.factory('apiService', function ($http) {
+			function search(objCode, query) {
+				return $http.jsonp(API_CONTEXT + '/' + objCode + '/search?jsonp=JSON_CALLBACK&' + query);
+			}
+
+			return {
+				search: search
+			};
 		});
 })();
