42413fd110103db3afcd185b17c1f9b672538aad
Brad Horrocks <horrocks.brad@gmail.com>
2014-02-27 10:47:51 -0700

    now with kitties

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 (function () {
 	var API_CONTEXT = 'https://hub.attask.com/attask/api-internal';
 
-	angular.module('TaskCaster', ['ngRoute'])
+	angular.module('TaskCaster', ['ui.bootstrap', 'ngRoute'])
 		.config(['$locationProvider', function ($locationProvider) {
 			$locationProvider.html5Mode(false);
 		}])
@@ -24,5 +24,20 @@
 			return {
 				search: search
 			};
-		});
+		})
+		.controller('CarouselDemoCtrl', function ($scope) {
+				$scope.myInterval = 5000;
+				var slides = $scope.slides = [];
+				$scope.addSlide = function() {
+					var newWidth = 600 + slides.length;
+					slides.push({
+						image: 'http://placekitten.com/' + newWidth + '/300',
+						text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+						['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+					});
+				};
+				for (var i=0; i<4; i++) {
+					$scope.addSlide();
+				}
+			});
 })();
